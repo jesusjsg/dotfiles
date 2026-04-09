293b671c4ef7111308859da9ad312d5060f8ffe1
jesus <jesusjsgdev@gmail.com>
2026-04-08 22:34:27 -0400

    feat: update treesitter config to branch main

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,3 +5,24 @@ require("config.keymaps")
 require("vim._core.ui2").enable()
 
 vim.cmd("packadd nvim.undotree")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "python", "lua", "bash", "vim", "markdown", "markdown_inline" },
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
+
+-- vim.api.nvim_create_autocmd("FileType", {
+--   callback = function()
+--     vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+--     vim.opt.foldmethod = "expr"
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
+--   pattern = "*",
+--   callback = function()
+--     vim.cmd("normal! zR")
+--   end,
+-- })
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,43 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  lazy = false,
+  branch = "main",
   build = ":TSUpdate",
+  lazy = false,
   dependencies = { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
   config = function()
-    require("nvim-treesitter").setup({
-      auto_install = false,
-      modules = {},
-      sync_install = false,
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "yaml",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "python",
-        "regex",
-        "toml",
-      },
-      ignore_install = { "tmux" },
-      highlight = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
+    local treesitter = require("nvim-treesitter")
+    treesitter.install({ "python", "bash", "lua", "markdown", "javascript", "typescript", "json", "vim" })
   end,
 }
