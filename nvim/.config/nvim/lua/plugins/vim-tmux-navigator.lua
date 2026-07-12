1339edf260fcb5e190cc637775b3c2c1c3a33586
jesus <jesusjsg03@gmail.com>
2026-07-12 10:00:30 -0400

    feat: update keymap to use Alt key to navigate between panes

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,7 +1,7 @@
 return {
   "christoomey/vim-tmux-navigator",
-  vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>"),
-  vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>"),
-  vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>"),
-  vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>"),
+  vim.keymap.set("n", "<M-h>", ":TmuxNavigateLeft<CR>"),
+  vim.keymap.set("n", "<M-j>", ":TmuxNavigateDown<CR>"),
+  vim.keymap.set("n", "<M-k>", ":TmuxNavigateUp<CR>"),
+  vim.keymap.set("n", "<M-l>", ":TmuxNavigateRight<CR>"),
 }
